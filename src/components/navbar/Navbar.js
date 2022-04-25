c9729b3cb4abe2254f842c7fe4764e92a09244d5
Snowkids-code <oumaj47@gmail.com>
2022-04-25 19:53:03 +0300

    Navbar frontend design done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,55 @@
-import './Navbar.scss'
+import {
+  ChatBubbleOutlineOutlined,
+  DarkModeOutlined,
+  FullscreenExitOutlined,
+  LanguageOutlined,
+  ListOutlined,
+  NotificationsNoneOutlined,
+  SearchOutlined,
+} from "@mui/icons-material";
+import "./Navbar.scss";
 
 const Navbar = () => {
-    return(
-        <div></div>
-    )
-}
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input placeholder="search" />
+          <SearchOutlined />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlined className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlined className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlined className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlined className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlined className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlined className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://media.istockphoto.com/photos/closeup-portrait-of-her-she-nice-attractive-puzzled-ignorant-girl-picture-id1132758418?k=20&m=1132758418&s=612x612&w=0&h=ca6WVDDblf3um3mdfCSGVpLGfwuyjj5UTLD9rMMHfH4="
+              alt="image"
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,14 @@
+import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
 
 const Home = () => {
   return <div className="home">
       <Sidebar/>
-      <div className="home_container">Container</div>
+      <div className="home_container">
+        <Navbar/>
+        Home
+      </div>
   </div>;
 };
 
