1b76f989c60eaca26eb4170cc1c1a917ffaa2e66
Snowkids-code <oumaj47@gmail.com>
2022-05-02 07:11:05 +0300

    Firebase login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,45 @@
-import './Login.scss'
+import { useState } from "react";
+import "./Login.scss";
+import { auth } from "../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    return(
-        <div>Login</div>
-    )
-}
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const navigate = useNavigate(); //useNaviagte used to move to a different page
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((credentials) => {
+        const user = credentials.user;
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
+  return (
+    <div className="login">
+      <form onSubmit={(e) => handleLogin(e)}>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && <span>Wrong email or password</span>}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
