ed771dcbee195b55083bf8ae55133a18899ea44e
Snowkids-code <oumaj47@gmail.com>
2023-06-21 14:56:01 +0300

    add firebase file

---
+++ src/firebase.js
@@ -0,0 +1,33 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+// import { getStorage } from "firebase/storage";
+// import { getAnalytics } from "firebase/analytics";
+// const {firebase} = require('firebase')
+const { getAuth } = require("firebase/auth");
+const { initializeApp } = require("firebase/app");
+const { getFirestore } = require("firebase/firestore");
+const { getStorage } = require("firebase/storage");
+
+const config = {
+  apiKey: "",
+  authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: "",
+  messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(config);
+
+const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
+// const analytics = getAnalytics(app);
+
+module.exports = {
+  db,
+  auth,
+  storage,
+};
