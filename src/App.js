658dd96479c70ff15be43271a4017fcc9001be4d
Snowkids-code <oumaj47@gmail.com>
2023-06-21 14:48:15 +0300

    user login

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const { currentUser } = useContext(AuthContext)
 
   const RequireAuth = ({ children }) => {
-    return currentUser ? children : <Navigate to="/login" />;
+    return !currentUser ? children : <Navigate to="/login" />;
   };
 
   // console.log(currentUser)
