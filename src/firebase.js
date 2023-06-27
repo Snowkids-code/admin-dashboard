10296a428c46df9cae7f531e66572f0410b3217f
Snowkids-code <oumaj47@gmail.com>
2023-06-27 06:13:49 +0300

    public folder cleanup

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,7 @@ const { getFirestore } = require("firebase/firestore");
 const { getStorage } = require("firebase/storage");
 
 const config = {
-  apiKey: process.env.API_KEY,
+  apiKey: "AIzaSyCcHWsKm2qaffiWAXSvrlWvFv7l11WoERE",
   authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: "",
